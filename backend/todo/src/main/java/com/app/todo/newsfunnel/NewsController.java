1e1b14131bed060bf0db4f2913fca24d1d966ad2
VU Quang Minh <vuquangminh2000317@gmail.com>
2021-10-09 18:02:58 +0800

    Added CrossOrigin and edited Get method for NewsController

--- backend/todo/src/main/java/com/app/todo/newsfunnel/NewsController.java
+++ backend/todo/src/main/java/com/app/todo/newsfunnel/NewsController.java
@@ -3,6 +3,7 @@ package com.app.todo.newsfunnel;
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+@CrossOrigin(origins="http://localhost:4200")
 @RestController
 public class NewsController {
     private NewsAPIService apiService;
@@ -23,10 +25,10 @@ public class NewsController {
     }
 
     // country = sg, query = anythingggg
-    @GetMapping("/news/{country}/{query}")
-    public List<News> getNews(@PathVariable(value = "country") String country,
+    @GetMapping("/news/{country_code}/{query}")
+    public List<News> getNews(@PathVariable(value = "country_code") String countryCode,
                         @PathVariable(value="query") String query) throws IOException, InterruptedException {
-        NewsAPIResponse newsResponse = apiService.getAPIResponse(country,query);
+        NewsAPIResponse newsResponse = apiService.getAPIResponse(countryCode,query);
 
         Gson gson = new Gson();
         NewsDTO[] news = new Gson().fromJson(newsResponse.getArticles(), NewsDTO[].class);
