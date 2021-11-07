4ef0bca891b86066ac6e3614d6598691509b20dd
VU Quang Minh <vuquangminh2000317@gmail.com>
2021-11-08 01:29:25 +0800

    Test CI

--- backend/todo/src/main/java/com/app/todo/scraper/ScraperConfig.java
+++ backend/todo/src/main/java/com/app/todo/scraper/ScraperConfig.java
@@ -16,9 +16,9 @@ public class ScraperConfig {
          * Path has to be set this way for CI. If running on VSCode without the Spring
          * Boot extension pack, add `backend/todo` in front of the path.
          */
-        System.setProperty("webdriver.chrome.driver", "backend/todo/src/main/resources/chromedriver.exe"); // Local Windows Repo
+//        System.setProperty("webdriver.chrome.driver", "backend/todo/src/main/resources/chromedriver.exe"); // Local Windows Repo
 //        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver"); // Mac
-//        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe"); // Windows
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe"); // Windows
 
     }
 
