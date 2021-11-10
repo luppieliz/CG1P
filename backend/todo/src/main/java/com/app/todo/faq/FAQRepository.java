bd1b1036271a18230a89c0a3c0f436b0d4e78539
Shawn Lee <leeswshawn@gmail.com>
2021-11-10 21:44:14 +0800

    Fixed a few missing UUIDs

--- backend/todo/src/main/java/com/app/todo/faq/FAQRepository.java
+++ backend/todo/src/main/java/com/app/todo/faq/FAQRepository.java
@@ -1,11 +1,12 @@
 package com.app.todo.faq;
 
+import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FAQRepository extends JpaRepository<FAQ, Long> {
+public interface FAQRepository extends JpaRepository<FAQ, UUID> {
 
     boolean existsByURL(String URL);
 }
--- backend/todo/src/main/java/com/app/todo/newsfunnel/NewsRepository.java
+++ backend/todo/src/main/java/com/app/todo/newsfunnel/NewsRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface NewsRepository extends JpaRepository<News, Long> {
+public interface NewsRepository extends JpaRepository<News, String> {
 
     boolean existsByTitle(String title);
     boolean existsByURL(String URL);
