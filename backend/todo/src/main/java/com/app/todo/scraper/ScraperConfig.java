624b4d317465cd60419e182e885e420131908afe
VU Quang Minh <vuquangminh2000317@gmail.com>
2021-11-10 22:36:24 +0800

    Resolved conflict

--- backend/todo/src/main/java/com/app/todo/scraper/ScraperConfig.java
+++ backend/todo/src/main/java/com/app/todo/scraper/ScraperConfig.java
@@ -17,8 +17,8 @@ public class ScraperConfig {
 //        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
 
         // For local IDE
-         System.setProperty("webdriver.chrome.driver", "backend/todo/src/main/resources/chromedriver.exe"); // Windows
-        // System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver"); // Mac
+//         System.setProperty("webdriver.chrome.driver", "backend/todo/src/main/resources/chromedriver.exe"); // Windows
+         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver"); // Mac
 
         chromeOptions.addArguments("--headless");
         return new ChromeDriver(chromeOptions);
