1107dc0a56ee94c15a7dd7b540fc9f67db579a12
Shawn Lee <leeswshawn@gmail.com>
2021-11-12 00:38:49 +0800

    Revert "Fixed Email CI"

--- backend/todo/src/main/java/com/app/todo/email/Email.java
+++ backend/todo/src/main/java/com/app/todo/email/Email.java
@@ -2,7 +2,6 @@ package com.app.todo.email;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.Map;
@@ -10,20 +9,12 @@ import java.util.Map;
 @Getter
 @Setter
 @AllArgsConstructor
-@NoArgsConstructor
 public class Email {
     private String from;
     private String mailTo;
-    private String[] mailToMultiple;
     private String subject;
     private String message;
     private Map<String, Object> props;
 
-    public Email(String from, String mailTo, String subject, String message, Map<String, Object> props) {
-        this.from = from;
-        this.mailTo = mailTo;
-        this.subject = subject;
-        this.message = message;
-        this.props = props;
-    }
+    public Email() {}
 }
